82702ea6dd52b603759008dcddf1f2f1e06003f8
Qianglong Mo <sammok2003@hotmail.com>
2015-02-12 01:19:01 +0800

    Fix ui mistake.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,8 +13,27 @@ $(function (){
                 var toTop = $('body').scrollTop();
                 for (var i = 0; i < anchorPosition.length; i++) {
                     if (toTop >= anchorPosition[i] - 300) {
-                        $('.product .item main').eq(i).addClass('animated flipInX');
+                        $('.product .item main').eq(i).addClass('animated flash');
                     }
+                }
+            });
+        },
+        service:function (){
+            function initType(){
+                $(".service").typed({
+                    strings: ['<dt>航拍服务按次数收费</dt><dd>航拍范围：高50米、出海50米<br />DJI无人机航拍服务：1000元/10分钟/次<br />每次航拍提供10分钟高清拍摄素材</dd>'],
+                    typeSpeed: 0,
+                    showCursor: false
+                });
+            }
+
+            var servicePosition = $('.section02').offset().top;
+
+            $(window).scroll(function (){
+                var toTop = $('body').scrollTop();
+
+                if (toTop >= servicePosition - 500){
+                    initType();
                 }
             });
         },
@@ -28,6 +47,7 @@ $(function (){
             }, 1000);
 
             app.productAnimate();
+            app.service();
         }
     };
 
